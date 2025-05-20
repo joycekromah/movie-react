dd03639031ee12777a43420ca8d58ed0564ae8a7
joycekromah <joycekromah@hotmail.com>
2025-05-20 18:05:54 +0200

    styling på li element

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,14 @@
 import React from "react";
 import StarIcon from "./MovieGrade";
+import "./App.css";
 
 
 export default function Movie(props) {
     return (
         <li className="list-group-item">
             {props.item.title}
+            <button id="delbtn" className="btn btn-sm btn-danger float-right" onClick={() => {props.deleteItem(props.item.id)}}>X</button>
             <StarIcon grade={props.item.grade}></StarIcon>
-            <button className="btn btn-sm btn-danger float-right" onClick={() => {props.deleteItem(props.item.id)}}>X</button>
         </li>
     )
 }
